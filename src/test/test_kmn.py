43caeb4bc678c09e63500b6cfd151e1cc4a8e8ca
arbackus <a.r.backus@gmail.com>
2017-06-04 20:15:16 +0200

    added test for external estimator, multiple session management in single test case

--- src/test/test_kmn.py
+++ src/test/test_kmn.py
@@ -2,11 +2,16 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import numpy as np
 from src.kmn import KernelMixtureNetwork
+from keras.layers import Dense
 
 
 class TestKernelMixtureNetwork(tf.test.TestCase):
 
     def create_dataset(self, n=5000):
+        """
+        function to create dummy data
+        source: http://edwardlib.org/tutorials/mixture-density-network
+        """
         y_data = np.random.uniform(-10.5, 10.5, n)
         r_data = np.random.normal(size=n)  # random noise
         x_data = np.sin(0.75 * y_data) * 7.0 + y_data * 0.5 + r_data * 1.0
@@ -15,7 +20,7 @@ class TestKernelMixtureNetwork(tf.test.TestCase):
         return train_test_split(x_data, y_data, random_state=42)
 
     def test_run(self):
-        """test case with simulated data and network training"""
+        """test case with simulated data and network training with default settings"""
 
         X_train, X_test, y_train, y_test = self.create_dataset()
 
@@ -30,7 +35,7 @@ class TestKernelMixtureNetwork(tf.test.TestCase):
         self.assertTrue(train_loss1 < 2.)
         self.assertTrue(kmn.test_loss[-1] < 3.)
 
-        kmn.partial_fit(X_train, y_train, n_epoch=100, eval_set=(X_test, y_test))
+        kmn.partial_fit(X_train, y_train, n_epoch=200, eval_set=(X_test, y_test))
         self.assertTrue(kmn.train_loss[-1] <= train_loss1)
 
         likelihoods = kmn.predict(X_test, y_test)
@@ -41,11 +46,31 @@ class TestKernelMixtureNetwork(tf.test.TestCase):
         score = kmn.score(X_test, y_test)
         self.assertTrue(abs(mean_loglik - score) < 0.01)
 
+        kmn.sess.close()
+
         # TODO:
         # test for sample()
         # test for predict_density()
         # test for plot_loss()
-        # test with external estimator and X_ph input
+
+    def test_external_estimator(self):
+        """test case with simulated data and network training with an external estimator"""
+
+        X_train, X_test, y_train, y_test = self.create_dataset()
+
+        kmn1 = KernelMixtureNetwork()
+        kmn1.fit(X_train, y_train, n_epoch=100)
+        kmn1.sess.close()
+
+        X_ph = tf.placeholder(tf.float32, [None, X_train.shape[1]])
+        x = Dense(15, activation='relu')(X_ph)
+        neural_network = Dense(15, activation='relu')(x)
+
+        kmn2 = KernelMixtureNetwork(estimator=neural_network, X_ph=X_ph)
+        kmn2.fit(X_train, y_train, n_epoch=200)
+        kmn2.sess.close()
+
+        self.assertTrue(abs(kmn1.train_loss[-1] - kmn2.train_loss[-1]) < 0.1)
 
     def test_sample_center_points(self):
         pass
