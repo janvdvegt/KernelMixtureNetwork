31358e4c6566ccc319b3057e8a3c6c3cef89e1c7
janvdvegt <jan.vegt@gmail.com>
2017-06-07 11:47:20 +0200

    Changed a lot of code and some notebooks

--- src/kmn.py
+++ src/kmn.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.base import BaseEstimator
 from edward.models import Categorical, Mixture, Normal
 from keras.layers import Dense, Dropout
+import math
 import edward as ed
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -62,7 +63,22 @@ def sample_center_points(y, method='all', k=100, keep_edges=False):
 class KernelMixtureNetwork(BaseEstimator):
 
     def __init__(self, n_samples=10, center_sampling_method='k_means', n_centers=20, keep_edges=False,
-                 init_scales='default', estimator=None, X_ph=None):
+                 init_scales='default', estimator=None, X_ph=None, train_scales=False):
+        """
+        Main class for Kernel Mixture Network
+
+        Args:
+            center_sampling_method: String that describes the method to use for finding kernel centers
+            n_centers: Number of kernels to use in the output
+            keep_edges: Keep the extreme y values as center to keep expressiveness
+            init_scales: List or scalar that describes (initial) values of bandwidth parameter
+            estimator: Keras or tensorflow network that ends with a dense layer to place kernel mixture output on top off,
+                       if None use a standard 15 -> 15 Dense network
+            X_ph: Placeholder for input to your custom estimator, currently only supporting one input placeholder,
+                  but should be easy to extend to a list of placeholders
+            train_scales: Boolean that describes whether or not to make the scales trainable
+            n_samples: Determine how many samples to return
+        """
 
         self.sess = ed.get_session()
         self.inference = None
@@ -80,8 +96,11 @@ class KernelMixtureNetwork(BaseEstimator):
 
         if init_scales is 'default':
             init_scales = np.array([1])
-        self.init_scales = init_scales
+
+        # Transform scales so that the softplus will result in passed init_scales
+        self.init_scales = [math.log(math.exp(s) - 1) for s in init_scales]
         self.n_scales = len(self.init_scales)
+        self.train_scales = train_scales
 
         self.fitted = False
 
@@ -114,12 +133,12 @@ class KernelMixtureNetwork(BaseEstimator):
             info_dict = self.inference.update(feed_dict={self.X_ph: X, self.y_ph: y})
 
             train_loss = info_dict['loss'] / len(y)
-            self.train_loss = np.append(self.train_loss, train_loss)
+            self.train_loss = np.append(self.train_loss, -train_loss)
 
             if eval_set is not None:
                 X_test, y_test = eval_set
                 test_loss = self.sess.run(self.inference.loss, feed_dict={self.X_ph: X_test, self.y_ph: y_test}) / len(y_test)
-                self.test_loss = np.append(self.test_loss, test_loss)
+                self.test_loss = np.append(self.test_loss, -test_loss)
 
             # only print progress for the initial fit, not for additional updates
             if self.fitted is False:
@@ -165,6 +184,7 @@ class KernelMixtureNetwork(BaseEstimator):
         """
         # create a placeholder for the target
         self.y_ph = y_ph = tf.placeholder(tf.float32, [None])
+        self.n_sample_ph = tf.placeholder(tf.int32, None)
 
         #  store feature dimension size for placeholder
         self.n_features = X.shape[1]
@@ -186,7 +206,7 @@ class KernelMixtureNetwork(BaseEstimator):
         self.locs_array = locs_array = tf.unstack(tf.transpose(tf.multiply(tf.ones((self.batch_size, n_locs)), locs)))
 
         # scales of the gaussian kernels
-        self.scales = scales = tf.nn.softplus(tf.Variable(self.init_scales, dtype=tf.float32, trainable=True))
+        self.scales = scales = tf.nn.softplus(tf.Variable(self.init_scales, dtype=tf.float32, trainable=self.train_scales))
         self.scales_array = scales_array = tf.unstack(tf.transpose(tf.multiply(tf.ones((self.batch_size, self.n_scales)), scales)))
 
         # kernel weights, as output by the neural network
@@ -198,11 +218,13 @@ class KernelMixtureNetwork(BaseEstimator):
         self.mixtures = mixtures = Mixture(cat=cat, components=components, value=tf.zeros_like(y_ph))
 
         # tensor to store samples
-        self.samples = mixtures.sample(sample_shape=self.n_samples)
+        #self.samples = mixtures.sample(sample_shape=self.n_samples)
+        self.samples = mixtures.sample()
 
         # store minmax of training target values for a sensible default grid for self.predict_density()
-        self.y_min = y.min()
-        self.y_max = y.max()
+        self.y_range = y.max() - y.min()
+        self.y_min = y.min() - 0.1 * self.y_range
+        self.y_max = y.max() + 0.1 * self.y_range
         # placeholder for the grid
         self.y_grid_ph = y_grid_ph = tf.placeholder(tf.float32)
         # tensor to store grid point densities
@@ -220,11 +242,11 @@ class KernelMixtureNetwork(BaseEstimator):
         fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 3))
 
         # plot train loss
-        plt.plot(np.arange(len(self.train_loss)), -self.train_loss, label='Train')
+        plt.plot(np.arange(len(self.train_loss)), self.train_loss, label='Train')
 
         if len(self.test_loss) > 0:
             # plot test loss
-            plt.plot(np.arange(len(self.test_loss)), -self.test_loss, label='Test')
+            plt.plot(np.arange(len(self.test_loss)), self.test_loss, label='Test')
 
         plt.legend(fontsize=20)
         plt.xlabel('epoch', fontsize=15)
